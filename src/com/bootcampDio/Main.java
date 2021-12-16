21d531a135931487a74f388bb27378250c98d270
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2021-12-16 11:01:45 -0300

    multiplication table done

--- src/com/bootcampDio/Main.java
+++ src/com/bootcampDio/Main.java
@@ -1,8 +1,29 @@
 package com.bootcampDio;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int chosenNumber;
+    int result;
+
+    System.out.println("Hello!");
+
+    System.out.println("Please type a number between 1 and 10 to generate the multiplication table of this number: ");
+      chosenNumber = scan.nextInt();
+
+    if(chosenNumber < 1 || chosenNumber > 10) {
+      System.out.println("Invalid number.");
+      return;
     }
+
+    System.out.println("Multiplication table of " + chosenNumber + " :");
+
+    for (int i = 1; i < 11; i++) {
+      result = chosenNumber * i;
+      System.out.println(chosenNumber + " x " + i + " = " + result);
+    }
+  }
 }
